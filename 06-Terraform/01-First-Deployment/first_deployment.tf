e9f436cb343e78af41f12b3446215905695f1cd9
dhathridjavali <d.dhathri97@gmail.com>
2021-01-22 12:38:44 +0000

    terraform/

--- 06-Terraform/01-First-Deployment/first_deployment.tf
+++ 06-Terraform/01-First-Deployment/first_deployment.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-#  access_key = "Access Key"
-#  secret_key = "Secrey Key"
+#  access_key = "AKIA3SBRP23QPLXZ5NPC"
+#  secret_key = "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"
   region     = "us-east-2"
 }
 
